cbecacd526573e8453bcf6ed21b0eeaf18829c24
Gaetrix <muhammadghiyatsarrahmaniey@mail.ugm.ac.id>
2025-06-27 21:54:29 +0700

    Update param

--- Task1/CL_BuckConverter/Param_plecs.m
+++ Task1/CL_BuckConverter/Param_plecs.m
@@ -1,29 +1,32 @@
-% Parameter dasar
-vin = 24;        % Input voltage [V]
-vo = 7;        % Output voltage [V]
+% Muhammad Ghiyats AR Rahmaniey (22/503896/TK/55089)
+% Parameter Close Loop
+% Will be placed in the same folder as CL_BuckConverter.plecs
+vin = 12.7;      % Input voltage [V]
+vo = 3.3;        % Output voltage [V]
 fs = 50e3;       % Switching frequency [Hz]
 R = 1.2;         % Load resistance [Ohm]
 dI = 0.2;        % Current ripple fraction
 dV = 0.02;       % Voltage ripple fraction
-Rloss = 0.001;	  % R losses
+Rloss = 0.001;	 % R losses
 
 
-% Duty cycle dan periode switching
+% Duty cycle
 D = vo / vin;
-Ts = 1 / fs;
 
-% Arus beban rata-rata dan ripple
+% Voltage and Current ripple
 Io = vo / R;
 Ir = Io * dI;    % Ripple current
 Vr = vo * dV;    % Ripple voltage
 
-% Induktansi dan kapasitansi hasil desain
+% Calculate Inductor and Capacitor
 L = vo * (1 - D) / (Ir * fs);
 C = vo * (1 - D) / (8 * Vr * L * fs^2);
 
-% KP dan KI
+% Calculate KP dan KI
 Fbp = fs/20;     % Bandwith Propotional
 Fbi = Fbp/10;    % Bandwith Integral
 Kp  = 2*pi*Fbp*C % Konstanta Proptional
 Ki  = 2*pi*Fbi*Kp% Konstanta Integral
 
+fprintf('Induktor : %.6f H\n', L);
+fprintf('Kapasitor: %.6f F\n', C);
